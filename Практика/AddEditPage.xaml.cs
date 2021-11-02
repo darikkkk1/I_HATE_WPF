14f53abeebfab61d661abde48e2b5533eb45f85a
darikkkk1 <73314815+darikkkk1@users.noreply.github.com>
2021-11-02 08:19:07 +0300

    Add files via upload

---
+++ Практика/AddEditPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практика
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _Currenthotel = new Hotel();
+        public AddEditPage(Hotel SelectedHotel)
+        {
+            InitializeComponent();
+            if (SelectedHotel != null)
+                _Currenthotel = SelectedHotel;
+
+            DataContext = _Currenthotel;
+            Country_CB.ItemsSource = ToursEntities.GetContext().Country.ToList();
+
+        }
+
+        private void DG_Hotels_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Country_CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Практика/Navigation.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практика
+{
+    /// <summary>
+    /// Логика взаимодействия для Navigation.xaml
+    /// </summary>
+    public partial class Navigation : Page
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonForHotels(object sender, RoutedEventArgs e)
+        {
+            Manager.MainContent.Navigate(new HotelPage());
+        }
+
+        private void ButtonForTours(object sender, RoutedEventArgs e)
+        {
+            Manager.MainContent.Navigate(new ToursPage());
+        }
+    }
+}
